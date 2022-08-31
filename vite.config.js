a7339ae235f6b17b8690c9653adf5994d970c3a4
gnana sai reddy <gnanasaireddy1998@gmail.com>
2022-08-31 09:13:04 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  base: "/threeJs-boilerPlate-vite/",
   server: { https: true },
   plugins: [mkcert()],
 });
